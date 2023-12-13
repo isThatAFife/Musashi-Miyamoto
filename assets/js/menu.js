752f8b3600b11b9dd410c7b0ab3c244686f68fb9
Dylan Knapp <knappd@tcd.ie>
2023-12-13 10:35:17 +0000

    chore: added comments to js file

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,10 @@
 // Code from Stack Overflow https://stackoverflow.com/questions/58193096/how-do-i-close-this-checkbox-menu-after-the-user-clicks-on-the-links-or-outside
 var checkbox = document.querySelector( '#nav-toggle' );
 var icon = document.querySelector( '#menuicon i' );
+
+/**
+ * Set checked property to false when user clicks any element except the menu button. Remove event listener from document.
+ */
 var listener = function( e ) {
   if( e.target != checkbox && e.target != icon ) {
     checkbox.checked = false;
@@ -8,6 +12,9 @@ var listener = function( e ) {
   }
 };
 
+/**
+ * If checkbox is checked, add an event listener to the document.
+ */
 checkbox.addEventListener( 'click', function(){
   if( this.checked ) {
     document.addEventListener( 'click', listener );
